039a88899e675ad699447e87c30b5f937a974ff4
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2025-05-10 01:32:24 +0300

    semestrovka upgraded speed

--- semester-work-1/src/main/java/itis/grp403/TimurSibgatullin/Main.java
+++ semester-work-1/src/main/java/itis/grp403/TimurSibgatullin/Main.java
@@ -10,14 +10,24 @@ public class Main {
             graph1.BFS(0);
             results1.add(graph1.getStats());
         }
-        CSVWriter.saveResultsToCSV("trees", results1);
+        CSVWriter.saveResultsToCSV("trees.txt", results1);
+
         List<Result> results2 = new ArrayList<>();
         for (int i = 1; i < 101; i++) {
-            Graph graph1 = new Graph(i * 100, (i * 1000));
+            Graph graph1 = new Graph(i * 100, (i * 100 * i * 100));
             graph1.BFS(0);
             System.out.println(i);
             results2.add(graph1.getStats());
         }
-        CSVWriter.saveResultsToCSV("mediumCompleteGraph", results2);
+        CSVWriter.saveResultsToCSV("mediumCompleteGraph.txt", results2);
+
+        List<Result> results3 = new ArrayList<>();
+        for (int i = 1; i < 101; i++) {
+            Graph graph1 = new Graph(i * 100, ((i * 100) * ((int) (Math.log(i * 100) / Math.log(2)))));
+            graph1.BFS(0);
+            System.out.println(i);
+            results3.add(graph1.getStats());
+        }
+        CSVWriter.saveResultsToCSV("fullCompleteGraph.txt", results3);
     }
 }

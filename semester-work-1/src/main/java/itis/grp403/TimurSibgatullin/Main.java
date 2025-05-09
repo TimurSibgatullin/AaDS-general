737708934f1e1669789ac8bcf75b0f3033bd9b89
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2025-05-10 01:33:15 +0300

    semestrovka upgraded speed

--- semester-work-1/src/main/java/itis/grp403/TimurSibgatullin/Main.java
+++ semester-work-1/src/main/java/itis/grp403/TimurSibgatullin/Main.java
@@ -6,27 +6,25 @@ public class Main {
     public static void main(String[] args) {
         List<Result> results1 = new ArrayList<>();
         for (int i = 1; i < 101; i++) {
-            Graph graph1 = new Graph(i * 100, (i * 100) - 1);
-            graph1.BFS(0);
-            results1.add(graph1.getStats());
+            Graph graph = new Graph(i * 100, (i * 100) - 1);
+            graph.BFS(0);
+            results1.add(graph.getStats());
         }
         CSVWriter.saveResultsToCSV("trees.txt", results1);
 
         List<Result> results2 = new ArrayList<>();
         for (int i = 1; i < 101; i++) {
-            Graph graph1 = new Graph(i * 100, (i * 100 * i * 100));
-            graph1.BFS(0);
-            System.out.println(i);
-            results2.add(graph1.getStats());
+            Graph graph = new Graph(i * 100, (i * 100 * i * 100));
+            graph.BFS(0);
+            results2.add(graph.getStats());
         }
         CSVWriter.saveResultsToCSV("mediumCompleteGraph.txt", results2);
 
         List<Result> results3 = new ArrayList<>();
         for (int i = 1; i < 101; i++) {
-            Graph graph1 = new Graph(i * 100, ((i * 100) * ((int) (Math.log(i * 100) / Math.log(2)))));
-            graph1.BFS(0);
-            System.out.println(i);
-            results3.add(graph1.getStats());
+            Graph graph = new Graph(i * 100, ((i * 100) * ((int) (Math.log(i * 100) / Math.log(2)))));
+            graph.BFS(0);
+            results3.add(graph.getStats());
         }
         CSVWriter.saveResultsToCSV("fullCompleteGraph.txt", results3);
     }

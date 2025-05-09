364601d75ff71805924d18227ab8930fcf2aff90
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2025-05-09 17:49:36 +0300

    semestrovka02

--- semester-work-1/src/main/java/itis/grp403/TimurSibgatullin/Main.java
+++ semester-work-1/src/main/java/itis/grp403/TimurSibgatullin/Main.java
@@ -1,12 +1,23 @@
 package itis.grp403.TimurSibgatullin;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            Graph graph1 = new Graph(10000, 10000);
+//        List<Result> results1 = new ArrayList<>();
+//        for (int i = 1; i < 101; i++) {
+//            Graph graph1 = new Graph(i * 100, (i * 100) - 1);
+//            graph1.BFS(0);
+//            results1.add(graph1.getStats());
+//        }
+//        CSVWriter.saveResultsToCSV("trees", results1);
+        List<Result> results2 = new ArrayList<>();
+        for (int i = 100; i < 101; i++) {
+            Graph graph1 = new Graph(i * 100, (i * 1000));
             graph1.BFS(0);
-            System.out.println(graph1.getStats());
+            System.out.println(i);
+            results2.add(graph1.getStats());
         }
+        CSVWriter.saveResultsToCSV("mediumCompleteGraph", results2);
     }
 }
